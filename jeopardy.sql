9c881bdaa76f08b840d1b1768a266016beb44931
jplarar <jplarar@gmail.com>
2014-11-21 19:08:06 -0600

    database

---
+++ jeopardy.sql
@@ -0,0 +1,100 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50620
+ Source Host           : localhost
+ Source Database       : jeopardy
+
+ Target Server Type    : MySQL
+ Target Server Version : 50620
+ File Encoding         : utf-8
+
+ Date: 11/21/2014 19:01:11 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `categorias`
+-- ----------------------------
+DROP TABLE IF EXISTS `categorias`;
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `clase_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `clase_id` (`clase_id`),
+  CONSTRAINT `clase_id` FOREIGN KEY (`clase_id`) REFERENCES `clases` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+--  Table structure for `clases`
+-- ----------------------------
+DROP TABLE IF EXISTS `clases`;
+CREATE TABLE `clases` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuario_id` (`usuario_id`),
+  CONSTRAINT `usuario_id` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+--  Table structure for `equipos`
+-- ----------------------------
+DROP TABLE IF EXISTS `equipos`;
+CREATE TABLE `equipos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `puntos` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+--  Table structure for `integrantes`
+-- ----------------------------
+DROP TABLE IF EXISTS `integrantes`;
+CREATE TABLE `integrantes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `equipo_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `equipo_id` (`equipo_id`),
+  CONSTRAINT `equipo_id` FOREIGN KEY (`equipo_id`) REFERENCES `equipos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+--  Table structure for `pistas`
+-- ----------------------------
+DROP TABLE IF EXISTS `pistas`;
+CREATE TABLE `pistas` (
+  `id` int(11) NOT NULL,
+  `puntos` int(11) NOT NULL,
+  `pista` varchar(255) NOT NULL,
+  `correcta` varchar(255) NOT NULL DEFAULT '',
+  `incorrectaUno` varchar(255) NOT NULL DEFAULT '',
+  `IncorrectaDos` varchar(255) NOT NULL DEFAULT '',
+  `IncorrectaTres` varchar(255) NOT NULL DEFAULT '',
+  `categoria_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `categoria_id` (`categoria_id`),
+  CONSTRAINT `categoria_id` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+--  Table structure for `usuarios`
+-- ----------------------------
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `activo` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+SET FOREIGN_KEY_CHECKS = 1;
